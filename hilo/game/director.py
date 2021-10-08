5b6aff2a8a478ba22fc9648388f57fd99da4bb60
Sean Compton <seancompton@byui.edu>
2021-10-08 10:56:11 -0600

    We have all the basic thing setup we just need to do some error checking.

--- hilo/game/director.py
+++ hilo/game/director.py
@@ -1,21 +1,31 @@
 #This file manages and directs the other classes
+#imports the class Player into our program.
+from game.player import Player
 
 class Director():
 
     def __init__(self):
-        pass
+        self.card = 0
+        self.previouse_card = 0
+        self.keep_playing = ""
+        self.player = Player()
+
         
+    #A function to start the game.
+    def start_game(self):
+        while self.player.keep_playing():
+            self.do_input()
+            self.do_updates()
+            self.do_outputs()
+            print()
+            print()
 
-    def start_game():
-        pass
+    def do_input(self):
+        self.player.deal_card()
+        self.player.player_guess()
 
-    def do_input():
-        pass
+    def do_updates(self):
+        self.player.calculate_score()
 
-    def do_outputs():
+    def do_outputs(self):
         pass
-
-    def do_updates():
-        pass
-    
-    
--- hilo/game/player.py
+++ hilo/game/player.py
@@ -1,18 +1,46 @@
 #This follows the player, and allows them to guess
+import random
 
 class Player():
 
     def __init__(self):
-        pass
+        self.score = 300
+        self.old_card = 0
+        self.new_card = 0
+        self.user_decision = ""
+        self.end_game = True
 
-    def keep_playing():
-        pass
+    def keep_playing(self):
+        if self.score < 0:
+            self.end_game = False
+        else:
+            self.user_decision = input("Would you like to keep playing(y/n)? ")
+            if self.user_decision == "y":
+                pass
+            else:
+                self.end_game = False
+        return self.end_game
+                
 
-    def player_guess():
-        pass
+    def player_guess(self):
+        self.user_decision = input("Higher or lower(h/l)? ")
 
-    def score():
-        pass
+    def calculate_score(self):
+        self.old_card = self.new_card
+        self.new_card = random.randint(1,14)
+        if self.user_decision.lower() == "h":
+            if self.new_card > self.old_card:
+                self.score = self.score + 100
+            else:
+                self.score = self.score - 75
+        else:
+            if self.new_card < self.old_card:
+                self.score = self.score + 100
+            else:
+                self.score = self.score - 75
+        print(f"The new card is {self.new_card}")
+        print(f"Your score is: {self.score}")
 
-    def end_game():
-        pass
+    def deal_card(self):
+        self.new_card = random.randint(1, 14)
+        print(f"The card is {self.new_card}")
